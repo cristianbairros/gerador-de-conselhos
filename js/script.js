c5d54b0709b301a859c0c80519857e5811d919ed
cristian bairros <cristianmil7hor@gmail.com>
2022-12-24 15:56:12 -0300

    novo repositório!

---
+++ js/script.js
@@ -0,0 +1,80 @@
+async function conselhosAleatorios(){
+const url = 'https://api.adviceslip.com/advice'
+const resposta = await fetch(url)
+const json = await resposta.json()
+let conselho = json.slip.advice
+printarConselhosAleatorios(conselho)
+}
+function printarConselhosAleatorios(conselho){
+document.querySelector('.conselhos').innerHTML = conselho
+}
+conselhosAleatorios()
+document.querySelector('.botao').addEventListener('click',()=>{
+conselhosAleatorios()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
